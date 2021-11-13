648d135ff4f95a4bdea8ee72e14a0014ef6dc8ff
Reekraj Roy <55712612+REEKRAJROY@users.noreply.github.com>
2021-11-13 20:59:46 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailsComponent } from './todo-details/todo-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    TodoDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
